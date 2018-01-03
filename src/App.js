4804a0779d425afc9af643bb2afcab356f44fb90
Daniel Oxman <zeoxyman@gmail.com>
2018-01-03 00:05:56 -0500

    Added second component via NPM to test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import TicTacToe from '@zeoxyman/tictactoe-react'
+import TicTacToe from '@zeoxyman/tictactoe-react';
+import SlidePuzzle from '@zeoxyman/slide-puzzle-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <TicTacToe />
+      <div className="container">
+        <TicTacToe />
+        <SlidePuzzle />
+      </div>
     );
   }
 }
